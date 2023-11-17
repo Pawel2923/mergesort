497840b241f942f1c91df87baece494e99519d2b
Paweł <pawelporemba123@gmail.com>
2023-11-17 18:30:47 +0100

    Dodanie spawdzenia sortowania odwrotnie

--- MergeSort-Test/test.cpp
+++ MergeSort-Test/test.cpp
@@ -3,10 +3,17 @@
 #include "pch.h"
 #include "MergeSort.h"
 
-static void mergeSort(int* array, int size)
+static void mergeSort(int* array, int size, bool reverse = false)
 {
     MergeSort sort(size);
-    sort.mergeSort(array, 0, size - 1);
+    if (reverse)
+    {
+		sort.mergeSortReverse(array, 0, size - 1);
+	}
+    else
+    {
+		sort.mergeSort(array, 0, size - 1);
+	}
 }
 
 static TEST(MergeSortTest, ArrayChanged) {
@@ -32,6 +39,25 @@ static TEST(MergeSortTest, ArrayChanged) {
 
     delete[] array;
     delete[] arrayCopy;
+}
+
+static TEST(MegeSortTest, IsReverseSorted)
+{
+    srand(time(NULL));
+
+    int size = rand() % 10000 + 5;
+    int* array;
+    array = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        array[i] = rand() % 10000 + 1;
+    }
+
+    mergeSort(array, size, true);
+
+    ASSERT_TRUE(std::is_sorted(array, array + size, std::greater<int>()));
+
+    delete[] array;
 }
 
 int main(int argc, char** argv)
